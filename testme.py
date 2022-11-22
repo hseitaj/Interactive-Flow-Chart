f82c68db48bbe93bd50de22487d8b7330f3dd42f
Hansi Seitaj <67917151+hseitaj@users.noreply.github.com>
2022-11-22 13:21:30 -0500

    Update and rename testme.py to Almaz - testme.py

--- testme.py
+++
@@ -1,9 +0,0 @@
-import sched, time
-s = sched.scheduler(time.time, time.sleep)
-def do_something(sc):
-    print("Doing stuff...")
-    # do your stuff
-    sc.enter(5, 1, do_something, (sc,))
-
-s.enter(5, 1, do_something, (s,))
-s.run()
